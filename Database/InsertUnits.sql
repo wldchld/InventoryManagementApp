0ab06a22afdf2eb9824765672a8c80b90bd333a8
Ville <villehannu@hotmail.com>
2013-02-19 11:46:47 +0200

    Database update in units

---
+++ Database/InsertUnits.sql
@@ -0,0 +1,46 @@
+INSERT INTO 'Unit' ('UnitID', 'UnitName', 'BaseUnitID', 'Factor', 'IsSI', 'Type') VALUES (1, 'pcs', 1, 1, 1, 1);
+
+INSERT INTO 'Unit' ('UnitID', 'UnitName', 'BaseUnitID', 'Factor', 'IsSI', 'Type') VALUES (2, 'g', 2, 1, 1, 2);
+
+INSERT INTO 'Unit' ('UnitID', 'UnitName', 'BaseUnitID', 'Factor', 'IsSI', 'Type') VALUES (3, 'kg', 2, 1000, 1, 2);
+
+INSERT INTO 'Unit' ('UnitID', 'UnitName', 'BaseUnitID', 'Factor', 'IsSI', 'Type') VALUES (4, 't', 2, 1000000, 1, 2);
+
+INSERT INTO 'Unit' ('UnitID', 'UnitName', 'BaseUnitID', 'Factor', 'IsSI', 'Type') VALUES (5, 'oz', 2, 28.34952, 0, 2);
+
+INSERT INTO 'Unit' ('UnitID', 'UnitName', 'BaseUnitID', 'Factor', 'IsSI', 'Type') VALUES (6, 'lb', 2, 453.59273, 0, 2);
+
+INSERT INTO 'Unit' ('UnitID', 'UnitName', 'BaseUnitID', 'Factor', 'IsSI', 'Type') VALUES (7, 'st', 2, 6350, 0, 2);
+
+INSERT INTO 'Unit' ('UnitID', 'UnitName', 'BaseUnitID', 'Factor', 'IsSI', 'Type') VALUES (8, 'l', 8, 1, 1, 3);
+
+INSERT INTO 'Unit' ('UnitID', 'UnitName', 'BaseUnitID', 'Factor', 'IsSI', 'Type') VALUES (9, 'dl', 8, 0.1, 1, 3);
+
+INSERT INTO 'Unit' ('UnitID', 'UnitName', 'BaseUnitID', 'Factor', 'IsSI', 'Type') VALUES (10, 'tsp', 8, 0.005, 1, 3);
+
+INSERT INTO 'Unit' ('UnitID', 'UnitName', 'BaseUnitID', 'Factor', 'IsSI', 'Type') VALUES (11, 'tbsp', 8, 0.015, 1, 3);
+
+INSERT INTO 'Unit' ('UnitID', 'UnitName', 'BaseUnitID', 'Factor', 'IsSI', 'Type') VALUES (12, 'pt', 8, 0.473, 0, 3);
+
+INSERT INTO 'Unit' ('UnitID', 'UnitName', 'BaseUnitID', 'Factor', 'IsSI', 'Type') VALUES (13, 'gal', 8, 3.7854, 0, 3);
+
+INSERT INTO 'Unit' ('UnitID', 'UnitName', 'BaseUnitID', 'Factor', 'IsSI', 'Type') VALUES (14, 'cm', 16, 0.01, 1, 4);
+
+INSERT INTO 'Unit' ('UnitID', 'UnitName', 'BaseUnitID', 'Factor', 'IsSI', 'Type') VALUES (15, 'dm', 16, 0.1, 1, 4);
+
+INSERT INTO 'Unit' ('UnitID', 'UnitName', 'BaseUnitID', 'Factor', 'IsSI', 'Type') VALUES (16, 'm', 16, 1, 1, 4);
+
+INSERT INTO 'Unit' ('UnitID', 'UnitName', 'BaseUnitID', 'Factor', 'IsSI', 'Type') VALUES (17, 'in', 16, 0.0254, 0, 4);
+
+INSERT INTO 'Unit' ('UnitID', 'UnitName', 'BaseUnitID', 'Factor', 'IsSI', 'Type') VALUES (18, 'ft', 16, 0.3048, 0, 4);
+
+INSERT INTO 'Unit' ('UnitID', 'UnitName', 'BaseUnitID', 'Factor', 'IsSI', 'Type') VALUES (19, 'yd', 16, 0.9144, 0, 4);
+
+INSERT INTO 'Unit' ('UnitID', 'UnitName', 'BaseUnitID', 'Factor', 'IsSI', 'Type') VALUES (20, 'mi', 16, 1609.344, 0, 4);
+
+INSERT INTO 'Unit' ('UnitID', 'UnitName', 'BaseUnitID', 'Factor', 'IsSI', 'Type') VALUES (21, 'pcs', 1, 1, 0, 1);
+
+INSERT INTO 'Unit' ('UnitID', 'UnitName', 'BaseUnitID', 'Factor', 'IsSI', 'Type') VALUES (22, 'tsp', 8, 0.005, 0, 3);
+
+INSERT INTO 'Unit' ('UnitID', 'UnitName', 'BaseUnitID', 'Factor', 'IsSI', 'Type') VALUES (23, 'tbsp', 8, 0.015, 0, 3);
+
--- Database/createDB.sql
+++ Database/createDB.sql
@@ -2,22 +2,26 @@
 -- Author:        ville
 -- Caption:       New Model
 -- Project:       Name of the project
--- Changed:       2013-02-18 19:45
+-- Changed:       2013-02-19 11:16
 -- Created:       2013-02-11 19:44
 PRAGMA foreign_keys = OFF;
 
 -- Schema: mydb
 BEGIN;
 CREATE TABLE "Unit"(
-  "UnitID" INTEGER PRIMARY KEY NOT NULL,
-  "UnitName" VARCHAR(45)
+  "UnitID" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+  "UnitName" VARCHAR(45) NOT NULL,
+  "BaseUnitID" INTEGER NOT NULL,
+  "Factor" FLOAT NOT NULL,
+  "IsSI" BOOLEAN NOT NULL,
+  "Type" INTEGER NOT NULL
 );
 CREATE TABLE "Recipe"(
-  "RecipeID" INTEGER PRIMARY KEY NOT NULL,
+  "RecipeID" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
   "RecipeName" VARCHAR(45)
 );
-CREATE TABLE "MaterialType"(
-  "MaterialTypeID" INTEGER PRIMARY KEY NOT NULL,
+CREATE TABLE "MaterialGroup"(
+  "MaterialGroupID" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
   "Name" VARCHAR(45)
 );
 CREATE TABLE "ExtraFields"(
@@ -27,32 +31,32 @@ CREATE TABLE "ExtraFields"(
   "StringExtra" VARCHAR(45),
   "FloatExtra" FLOAT,
   "DateExtra" DATE,
-  "MaterialTypeID" INTEGER NOT NULL,
+  "MaterialGroupID" INTEGER NOT NULL,
   CONSTRAINT "fk_ExtraFields_MaterialType1"
-    FOREIGN KEY("MaterialTypeID")
-    REFERENCES "MaterialType"("MaterialTypeID")
+    FOREIGN KEY("MaterialGroupID")
+    REFERENCES "MaterialGroup"("MaterialGroupID")
 );
-CREATE INDEX "ExtraFields.fk_ExtraFields_MaterialType1_idx" ON "ExtraFields"("MaterialTypeID");
+CREATE INDEX "ExtraFields.fk_ExtraFields_MaterialType1_idx" ON "ExtraFields"("MaterialGroupID");
 CREATE TABLE "ShoppingList"(
-  "ShoppingLIstID" INTEGER PRIMARY KEY NOT NULL,
+  "ShoppingLIstID" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
   "ShoppingListName" VARCHAR(45) NOT NULL
 );
 CREATE TABLE "Material"(
-  "MaterialID" INTEGER PRIMARY KEY NOT NULL,
+  "MaterialID" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
   "MaterialName" VARCHAR(45),
   "UnitID" INTEGER NOT NULL,
-  "MaterialTypeID" INTEGER NOT NULL,
+  "MaterialGroupID" INTEGER NOT NULL,
   CONSTRAINT "fk_Material_Unit1"
     FOREIGN KEY("UnitID")
     REFERENCES "Unit"("UnitID"),
   CONSTRAINT "fk_Material_table11"
-    FOREIGN KEY("MaterialTypeID")
-    REFERENCES "MaterialType"("MaterialTypeID")
+    FOREIGN KEY("MaterialGroupID")
+    REFERENCES "MaterialGroup"("MaterialGroupID")
 );
 CREATE INDEX "Material.fk_Material_Unit1_idx" ON "Material"("UnitID");
-CREATE INDEX "Material.fk_Material_table11_idx" ON "Material"("MaterialTypeID");
+CREATE INDEX "Material.fk_Material_table11_idx" ON "Material"("MaterialGroupID");
 CREATE TABLE "Inventory"(
-  "RecID" INTEGER PRIMARY KEY NOT NULL,
+  "RecID" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
   "Amount" INTEGER NOT NULL,
   "MaterialID" INTEGER NOT NULL,
   CONSTRAINT "fk_Inventory_Material"
@@ -61,7 +65,7 @@ CREATE TABLE "Inventory"(
 );
 CREATE INDEX "Inventory.fk_Inventory_Material_idx" ON "Inventory"("MaterialID");
 CREATE TABLE "RecipeEntry"(
-  "RecID" VARCHAR(45) PRIMARY KEY NOT NULL,
+  "RecID" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
   "RecipeID" INTEGER NOT NULL,
   "Amount" INTEGER,
   "MaterialID" INTEGER NOT NULL,
@@ -75,7 +79,7 @@ CREATE TABLE "RecipeEntry"(
 CREATE INDEX "RecipeEntry.fk_RecipeContent_Recipe1_idx" ON "RecipeEntry"("RecipeID");
 CREATE INDEX "RecipeEntry.fk_RecipeContent_Material1_idx" ON "RecipeEntry"("MaterialID");
 CREATE TABLE "ShoppingListEntry"(
-  "RecID" VARCHAR(45) PRIMARY KEY NOT NULL,
+  "RecID" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
   "ShoppingLIstID" INTEGER NOT NULL,
   "MaterialID" INTEGER NOT NULL,
   "Amount" INTEGER,
